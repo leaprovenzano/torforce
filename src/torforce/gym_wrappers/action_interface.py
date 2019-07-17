5f9f4da37af3833987e3e1e148a243675faba690
lea <leaprovenzano@gmail.com>
2019-07-17 08:00:27 +0100

    let tensorization be handled by superclass

--- src/torforce/gym_wrappers/action_interface.py
+++ src/torforce/gym_wrappers/action_interface.py
@@ -107,11 +107,11 @@ class ScaledActionInterface(ContinuiousActionInterface):
         self._internal_action_range = self._get_action_range(env)
         self.scaled_action_range = scaled_action_range
         self.action_range = scaled_action_range
-        self.scaler = MinMaxScaler( self._internal_action_range, self.scaled_action_range)
+        self.scaler = MinMaxScaler(self._internal_action_range, self.scaled_action_range)
 
     def tensor_to_action(self, x: torch.FloatTensor) -> np.ndarray:
-        return self.scaler(np.asarray(x))
+        return self.scaler(super().tensor_to_action(x))
 
     def action_to_tensor(self, x: np.ndarray) -> torch.FloatTensor:
-        return torch.FloatTensor(self.scaler.inverse_scale(x))
+        return super().action_to_tensor(self.scaler.inverse_scale(x))
 
