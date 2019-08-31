5e3cd2ab2c048449793d9fe67f5aafcd2065c783
lea <leaprovenzano@gmail.com>
2019-08-31 17:31:04 +0100

    fix pytorch to 1.1 until we're ready to deal w/ breaking changes

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = ['torch>=1.1.0', 'gym>=0.12.4']
+requirements = ['torch>=1.1,<1.2.0', 'gym>=0.12.4']
 
 setup_requirements = ['pytest-runner']
 
