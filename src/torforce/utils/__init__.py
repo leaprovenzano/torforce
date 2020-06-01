2c8923920bd0f990d3b543fd5c63a89e16a57a5f
lea <leaprovenzano@gmail.com>
2020-06-01 07:51:20 +0100

    add classproperty

--- src/torforce/utils/__init__.py
+++ src/torforce/utils/__init__.py
@@ -1,4 +1,5 @@
 """general utilities torforce uses (mostly internally)"""
 
 from .scalers import MinMaxScaler
-from .nn_utils import *
+from .nn_utils import get_output_shape
+from .generic import classproperty
