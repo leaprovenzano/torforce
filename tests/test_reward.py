dd17824fed5c633658eaabe7d292ba821931f8c1
lea <leaprovenzano@gmail.com>
2020-06-06 12:44:45 +0100

    fix reward tests to use bool tensor for mask

--- tests/test_reward.py
+++ tests/test_reward.py
@@ -2,7 +2,7 @@ import pytest
 
 import torch
 
-from hypothesis import given, example
+from hypothesis import given
 from hypothesis import strategies as st
 
 from tests.strategies.torchtensors import float_tensors, tensors
@@ -11,7 +11,7 @@ from torforce.reward import discount
 
 
 def get_groups(mask):
-    groups = mask.flip(0).cumsum(0).flip(0)
+    groups = (mask * 1).flip(0).cumsum(0).flip(0)
     return abs(groups - groups.max())
 
 
@@ -36,55 +36,54 @@ def group_split(x, mask, return_mask=False):
 def discount_case(draw, rewards):
     reward = draw(rewards)
     shape = tuple(reward.shape)
-    terminals = draw(tensors('uint8', shape, elements=st.booleans()))
+    terminals = draw(tensors('bool', shape, elements=st.booleans()))
     return reward, terminals
 
 
 class TestDiscount:
 
-    basic_reward_strat = float_tensors(min_dims=1,
-                                       max_dims=2,
-                                       min_side=1,
-                                       max_side=10,
-                                       elements=st.floats(-100, 100, width=32))
-    ones = float_tensors(min_dims=1,
-                         max_dims=2,
-                         min_side=1,
-                         max_side=10,
-                         elements=st.floats(1, 1, width=32))
+    basic_reward_strat = float_tensors(
+        min_dims=1, max_dims=2, min_side=1, max_side=10, elements=st.floats(-100, 100, width=32)
+    )
+    ones = float_tensors(
+        min_dims=1, max_dims=2, min_side=1, max_side=10, elements=st.floats(1, 1, width=32)
+    )
 
     @given(basic_reward_strat)
     def test_simple_gamma_one(self, inp):
         terms = torch.zeros_like(inp)
-        terms[-1] = 1.
+        terms[-1] = 1
+        terms = terms != 0
         expected = inp.flip(0).cumsum(0).flip(0)
-        result = discount(inp, terminals=terms, gamma=1.)
+        result = discount(inp, terminals=terms, gamma=1.0)
 
         torch.testing.assert_allclose(result, expected, rtol=0.0001, atol=0.0001)
 
     @given(basic_reward_strat)
     def test_simple_gamma_zero(self, inp):
         terms = torch.zeros_like(inp)
-        terms[-1] = 1.
-        result = discount(inp, terminals=terms, gamma=0.)
+        terms[-1] = 1
+        terms = terms != 0
+        print(terms)
+        result = discount(inp, terminals=terms, gamma=0.0)
 
         torch.testing.assert_allclose(result, inp, rtol=0.0001, atol=0.0001)
 
     @given(discount_case(ones))
     def test_mult_term_ones(self, inp):
-        gamma = .99
+        gamma = 0.99
         rewards, terms = inp
         result = discount(rewards, terms, gamma=gamma, bootstrap=1)
         torch.testing.assert_allclose(result[terms], rewards[terms])
 
-        if not (terms == 1).all():
+        if not terms.all():
             for grp, msk in group_split(result, mask=terms, return_mask=True):
 
                 expected = 1
 
-                if msk[-1].item() == 0:
+                if not msk[-1].item():
                     expected += 1 * gamma
 
                 for v in grp.flip(0).tolist():
-                    pytest.approx(v, .00001) == expected
+                    pytest.approx(v, 0.00001) == expected
                     expected += 1 + (expected * gamma)
